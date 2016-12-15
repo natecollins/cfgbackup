bc4bc1e743afa423b46942620b4a0a83e3b70f31
Nate Collins <npcollins@gmail.com>
2016-12-15 00:19:40 -0500

    Help msg

---
+++ parseconf.bash
@@ -0,0 +1,14 @@
+###############################
+# Parse Config File
+###############################
+
+###############################
+## Parse config file given
+## Returns 0 on success, 1 on error
+## Any errors will be in PARSE_ERRORS
+parse_config() {
+    PARSE_RET=0
+    declare -a PARSE_ERRORS
+
+    return $PARSE_RET
+}
