731e947081d8de41fd99d23b420147f74315095d
Nate Collins <npcollins@gmail.com>
2017-01-25 03:14:20 -0500

    Minor tweaks

--- runjob.bash
+++ runjob.bash
@@ -56,15 +56,22 @@ runjob_sync() {
     SYNC_FROM=$( epath_join ${CONFIG[SOURCE_DIR]} )
     SYNC_TO=$( epath_join ${CONFIG[TARGET_DIR]} )
 
+    RSYNC_FLAGS="${CONFIG[RSYNC_FLAGS]}"
     # Exclude PID_FILE from being synced
-    #TODO
+    RSYNC_FLAGS="${RSYNC_FLAGS} --exclude=/${PID_FILE}"
+
+    #TODO rsync command and exit code check
+
+    # Update timestamp of target dir to indicate backup time
+    touch $SYNC_TO
 }
 
 ###############################
 ## Run a rotate job
 runjob_rotatation() {
     log_entry "| Job type: rotation"
-    NEW_RUNDIR=$( rotate_start )
+    rotate_start
+    NEW_RUNDIR=$?
 
     RSYNC_FLAGS="${CONFIG[RSYNC_FLAGS]}"
 
@@ -72,17 +79,19 @@ runjob_rotatation() {
         # Get previous directory for target of link-dest, or skip if no previous backup dir
         PREV_BACKUP=$( rotate_current_backup )
         if [[ $PREV_BACKUP != "" ]]; then
+            RSYNC_FLAGS="${RSYNC_FLAGS} --link-dest=${PREV_BACKUP}"
             # If using old version of rsync (prior to 3.1.0), we must manually link files from
-            # the previous backup dir; version 3.1.0 and later can just use link-dest flag.
-            if rsync_gte_310; then
-                RSYNC_FLAGS="${RSYNC_FLAGS} --link-dest=${PREV_BACKUP}"
-            else
+            # the previous backup dir; version 3.1.0 and later works with just the link-dest flag above
+            if ! rsync_gte_310; then
                 #TODO manual link from prev backups
             fi
         fi
     fi
 
-    #TODO
+    #TODO rsync command and exit code check
+
+    # Update timestamp of target dir to indicate backup time
+    touch $RUN_DIR
 
     rotate_complete
 }
